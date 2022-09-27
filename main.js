5e6107328513ce3a030ba1ed94498bcaac431ed7
BeatrizAguillera <beatriz.aguillera@usp.br>
2022-09-27 01:58:57 -0300

    Squashed commit of the following:
    commit f91df3ac12a821ec2a5fdf257e0adfab05f3ed9e
    Author: Andy <andy@Andys-MacBook-Pro.local>
    Date:   Fri Apr 9 15:50:00 2021 -0400
    
        update cdn link
    
    commit 0a13d4e6077cedabe41fd0a671693915d1db1594
    Author: Adam Jahr <adamjahr@gmail.com>
    Date:   Thu Nov 12 16:54:59 2020 -0500
    
        Update index.html
    
    commit 1572b8c23e878c5822d4e03a02d5eee1151c7221
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Tue Jul 21 00:06:01 2020 -0400
    
        L6 starting
    
    commit 12062da9d1724afebf5a21a4ded3acb760a82bd1
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Wed Jun 24 01:50:31 2020 -0400
    
        L6 starting code
    
    commit 7f52372b6cde3fd8b5fdcc7502e8bf19b0e51793
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Wed Jun 24 01:48:12 2020 -0400
    
        L6 ending code
    
    commit 2bd501d071f30402046ef15d39aa4bf804f2497a
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Fri Jun 19 19:45:00 2020 -0400
    
        L5 ending code
    
    commit a6209b82c8c9fecc7be39548ad93dab32e62da41
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Fri Jun 19 19:41:08 2020 -0400
    
        L5 starting code
    
    commit 9ce75d24c21589ca0f5ca53e188ed7dfb40b1de1
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Fri Jun 19 19:36:48 2020 -0400
    
        L5 ending code
    
    commit a903dd19cbd4e23ffe6ba1f9bd69c878a1dfbc45
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Fri Jun 19 17:23:40 2020 -0400
    
        L5 starting code
    
    commit 2846669a3f416eaa3dbc1822e3a749a2ab649e85
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Tue Jun 2 21:21:37 2020 -0400
    
        L4 ending code
    
    commit b92a6c065c54a147c91c3b1d28a42e973e624691
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Tue Jun 2 20:33:19 2020 -0400
    
        L4 starting code
    
    commit 0f08a0ddb33b1c43b306abb07f1b6f4f0dbbb677
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Fri May 29 22:12:52 2020 -0400
    
        L3 ending code
    
    commit 7db826380e8845bc2e168c6a622799e0cbedf0f4
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Fri May 29 22:04:13 2020 -0400
    
        L3 starting code
    
    commit 95bb5ecd554d171ebf8aff97d3adb6e6555bbc8e
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Tue May 26 23:45:05 2020 -0400
    
        L3 start
    
    commit 39452ddc6cae0945a60082c4e851bac05d37562a
    Author: Adam Jahr <adamjahr@Adams-iMac.local>
    Date:   Tue May 26 23:39:42 2020 -0400
    
        lesson 3 ending

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const app = Vue.createApp({
             details: ['50% cotton', '30% wool', '20% polyester'],
             sizes: ['P', 'M', 'G'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ],
         }
     }
