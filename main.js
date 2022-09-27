9ca1604b20d6dbfac5e83710ae20e8f3cf4d3539
BeatrizAguillera <beatriz.aguillera@usp.br>
2022-09-27 20:20:36 -0300

    L7 challenge done!
    Some updates in the order of elements made too.
    In the next steps, I also must change it's style for the better.

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             link: 'https://www.vuemastery.com',
             onSale: true,
-            inventory: 15,
+            inventory: 70,
             details: ['50% cotton', '30% wool', '20% polyester'],
             sizes: ['P', 'M', 'G'],
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2235, color: 'darkblue', image: './assets/images/socks_blue.jpg' },
             ],
         }
     },
